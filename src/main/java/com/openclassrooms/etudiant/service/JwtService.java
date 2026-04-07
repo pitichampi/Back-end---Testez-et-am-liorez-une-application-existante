0d9ef1c2064146f7833e16b1a0364ca66ddfd8dd
Pierre <contact@pitichampi.fr>
2026-04-07 11:02:46 +0200

    Génération du Token
    Ajout des dépendances io.jsonwebtoken dans le projet (pom.xml)
    
    Appel des bonnes dépendances pour la génération du token : jsonwebtoken et les utilitaires standards Key et Date de java.
    
    Implémentation de la génération du token.
    
    ATTENTION : Cette méthode n’est PAS recomandée en production. Vu que c’est un exercice on en restera là mais pour une vraie solution il faudra penser à améliorer le système.

--- src/main/java/com/openclassrooms/etudiant/service/JwtService.java
+++ src/main/java/com/openclassrooms/etudiant/service/JwtService.java
@@ -1,14 +1,32 @@
 package com.openclassrooms.etudiant.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
+import java.util.Date;
+
 @Service
 public class JwtService {
 
-    public String generateToken(UserDetails userDetails) {
-        return null; // TODO
+    // D'abord, on crée une clef secrète
+    private final String SECRET = "+04@7noillebeR&enuLeDelluB:terceSnEesoRenUemiAlIuQervuoceDiuQdraneRnUDelleBserTeriotsiHLtsEC";
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
     }
 
+    // Et on génère le token PROPREMENT
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername()) // login
+                .setIssuedAt(new Date())  // Créé maintenant
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1h c'est déjà pas mal
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)  // Ca devrait suffire
+                .compact();
+    }
 }
